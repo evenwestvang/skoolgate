2c4dbf89ac5cef264d439ee4ba9d307cc9829ced
Even Westvang <even@bengler.no>
2010-11-02 00:26:13 +0100

    Tidied

--- app.rb
+++ app.rb
@@ -11,15 +11,19 @@ get '/get_markers/:lat/:lon/:lat2/:lon2/:detail_level' do
   else
     objects = Municipality.where(:location.within => {"$box" => box}).only(:name, :location, :result_average, :student_body_count)
   end
-  objects.map { |s| {:id => s.id, :body => s.student_body_count, :name => s.name, :avg => s.result_average, :lat => s.location[0], :lon => s.location[1]}}.to_json
+  objects.map { |s| {
+    :id => s.id, 
+    :body => s.student_body_count, 
+    :name => s.name, 
+    :avg => s.result_average, 
+    :lat => s.location[0], 
+    :lon => s.location[1]}}.to_json
 end
 
 get '/stylesheet.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass :stylesheet, :style => :compact
 end
-
-# <a href="http://github.com/you"><img style="position: absolute; top: 0; left: 0; border: 0;" src="http://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png" alt="Fork me on GitHub" /></a>
 
 helpers do
   def link_to(*args)
@@ -28,7 +32,6 @@ helpers do
       when 2 then text, url = args
       when 3 then text, url, opts = args
     end
-    
     opts ||= {}
     attributes = ""
     opts.each { |key,value| attributes << key.to_s << "=\"" << value << "\" "}
